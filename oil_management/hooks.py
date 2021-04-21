52b48ad16f0de09405a57f68a2a007d71ade888d
Hisham Ali Alshami <ahishamali10@gmail.com>
2021-04-21 22:35:00 +0300

    apply rtl style

--- oil_management/hooks.py
+++ oil_management/hooks.py
@@ -15,11 +15,11 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/oil_management/css/oil_management.css"
+app_include_css = "/assets/css/oil_management.min.css"
 # app_include_js = "/assets/oil_management/js/oil_management.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/oil_management/css/oil_management.css"
+web_include_css = "/assets/css/oil_management.min.css"
 # web_include_js = "/assets/oil_management/js/oil_management.js"
 
 # include js in page
@@ -35,7 +35,7 @@ app_license = "MIT"
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+home_page = "login"
 
 # website user home page (by Role)
 # role_home_page = {
